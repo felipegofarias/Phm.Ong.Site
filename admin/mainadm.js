dd35e1aa461c99038741cb0aa2a2fef8c8c7c04c
Felipe Gonçalves <farias.gfelipe@gmail.com>
2022-06-17 11:32:06 -0300

    Add files via upload

--- admin/mainadm.js
+++ admin/mainadm.js
@@ -1,65 +1,56 @@
-$(document).ready( function () {
-    $('#table_id').DataTable();
+
+$(document).ready( function() {
+    $('#table_idd').DataTable({
+      ajax: `https://localhost:4438/Usuario/ListarTodos`,
+      columns: [
+          { data: 'id' },
+          { data: 'cpf' },
+          { data: 'nome' },
+          { data: 'email' },
+          { data: 'senha' },
+          { data: 'dataAtualizacao' },
+          { data: 'ativo' },
+          { data: 'dataCadastro' }
+      ]
+  });
 } );
 
-const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-            datasets: [{
-                label: 'Doações',
-                data: [12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(75, 192, 192, 0.4)'
-                ],
-                borderColor: [
-                    'rgba(75, 192, 192, 1)'
-                ],
-                borderWidth: 3
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
 
-    
+function addNewUser(){
+  axios.post(`https://localhost:44384/Usuario/Cadatrar`, {
+   cpf : document.getElementById("cpf").value,
+   nome  : document.getElementById("nome").value,
+   email : document.getElementById("email").value,
+   senha : document.getElementById("senha").value
+  }) 
+        .then(function (response) {
+          
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
 
-    const ctxx = document.getElementById('chart').getContext('2d');
-    const chart = new Chart(ctxx,{
-      type: 'doughnut',
-      data:  {
-          labels: [
-              'Red',
-              'Blue',
-              'Yellow'
-          ],
-          datasets: [{
-            label: 'My First Dataset',
-            data: [300, 50, 100],
-            backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-            ],
-          hoverOffset: 4
-        }]
-      }
-    });
+}
 
+function addProjeto(){
+  event.preventDefault()
+  axios.post(`https://localhost:44384/Projeto/Cadatrar`, {
+   nome : document.getElementById("nome").value,
+   estado  : document.getElementById("estado").value,
+   cidade : document.getElementById("cidade").value
+   
+  }) 
+        .then(function (response) {
+          
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
 
-
-$(function() {
-    $("#calendario").datepicker();
-});
+}
 
 
 
 
-
-    
