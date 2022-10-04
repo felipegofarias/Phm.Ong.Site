f413ab775aad2a230c76a7f629b0a05ff98fc78f
Felipe Gonçalves <farias.gfelipe@gmail.com>
2022-10-04 10:10:41 -0300

    Update mainadm.js

--- admin/mainadm.js
+++ admin/mainadm.js
@@ -1,7 +1,7 @@
 
 $(document).ready( function() {
     $('#table_idd').DataTable({
-      ajax: `https://localhost:4438/Usuario/ListarTodos`,
+      ajax: `https://localhost:teste/Usuario/ListarTodos`,
       columns: [
           { data: 'id' },
           { data: 'cpf' },
@@ -17,7 +17,7 @@ $(document).ready( function() {
 
 
 function addNewUser(){
-  axios.post(`https://localhost:44384/Usuario/Cadatrar`, {
+  axios.post(`https://localhost:teste/Usuario/Cadatrar`, {
    cpf : document.getElementById("cpf").value,
    nome  : document.getElementById("nome").value,
    email : document.getElementById("email").value,
@@ -35,7 +35,7 @@ function addNewUser(){
 
 function addProjeto(){
   event.preventDefault()
-  axios.post(`https://localhost:44384/Projeto/Cadatrar`, {
+  axios.post(`https://localhost:teste/Projeto/Cadatrar`, {
    nome : document.getElementById("nome").value,
    estado  : document.getElementById("estado").value,
    cidade : document.getElementById("cidade").value
